42e97d3052261f4935f64d0285fae401b8942897
Diana Müller <diana.muller@globalhitss.com.br>
2024-09-04 15:14:18 -0300

    Comparator

--- collections-java-api/src/list/Ordenacao/OrdenacaoPessoa.java
+++ collections-java-api/src/list/Ordenacao/OrdenacaoPessoa.java
@@ -11,14 +11,31 @@ public class OrdenacaoPessoa {
         this.pessoaLista = new ArrayList<>();
     }
 
-    public void adicionarPessoa(String nome, int idade, int altura){
-        pessoaLista.add(new Pessoa(nome, idade, altura));
+    public void adicionarPessoa(String nome, int idade, double d) {
+        pessoaLista.add(new Pessoa(nome, idade, d));
     }
-    
-    public List<Pessoa> ordenarPessoaPorIdade(){
+
+    public List<Pessoa> ordenarPessoaPorIdade() {
         List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoaLista);
         Collections.sort(pessoasPorIdade);
         return pessoasPorIdade;
     }
-    
+
+    public List<Pessoa> ordenarPorAltura() {
+        List<Pessoa> pessoasPorAltura = new ArrayList<>(pessoaLista);
+        Collections.sort(pessoasPorAltura, new ComparatorPorAltura());
+        return pessoasPorAltura;
+    }
+
+    public static void main(String[] args) {
+        OrdenacaoPessoa ordenacaoPessoa = new OrdenacaoPessoa();
+
+        ordenacaoPessoa.adicionarPessoa("Nome 1", 23, 1.79);
+        ordenacaoPessoa.adicionarPessoa("Nome 2", 13, 1.48);
+        ordenacaoPessoa.adicionarPessoa("Nome 3", 3, 0.89);
+        ordenacaoPessoa.adicionarPessoa("Nome 4", 43, 1.78);
+
+        System.out.println(ordenacaoPessoa.ordenarPessoaPorIdade());
+        System.out.println(ordenacaoPessoa.ordenarPorAltura());
+    }
 }
--- collections-java-api/src/list/Ordenacao/Pessoa.java
+++ collections-java-api/src/list/Ordenacao/Pessoa.java
@@ -1,10 +1,11 @@
 package list.Ordenacao;
 
+import java.util.Comparator;
+
 public class Pessoa implements Comparable<Pessoa> {
     private String nome;
     private int idade;
     private double altura;
-
     
     public Pessoa(String nome, int idade, double altura) {
         this.nome = nome;
@@ -16,7 +17,7 @@ public class Pessoa implements Comparable<Pessoa> {
     public int compareTo(Pessoa p) {
         return Integer.compare(idade, p.getIdade());
     }
-    
+
     public String getNome() {
         return nome;
     }
@@ -37,5 +38,11 @@ public class Pessoa implements Comparable<Pessoa> {
                 ", altura = " + altura +
                 '}';
     }
-
+}
+class ComparatorPorAltura implements Comparator<Pessoa> {
+    @Override
+    public int compare(Pessoa p1, Pessoa p2) {
+       return Double.compare(p1.getAltura(), p2.getAltura());
+    }
+    
 }
