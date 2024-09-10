3371e11392779271144814c110f2137f4eb000f1
Diana Müller <diana.muller@globalhitss.com.br>
2024-09-10 11:48:06 -0300

    Herança - Classe Conteudo

--- desafio-poo-bootcamp-java/src/br/com/bootcamp/desafio/dominio/Curso.java
+++ desafio-poo-bootcamp-java/src/br/com/bootcamp/desafio/dominio/Curso.java
@@ -1,28 +1,16 @@
 package br.com.bootcamp.desafio.dominio;
 
-public class Curso {
-    private String titulo;
-    private String descricao;
+public class Curso extends Conteudo {
+
     private int cargaHoraria;
 
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
     }
 
     public void setCargaHoraria(int cargaHoraria) {
         this.cargaHoraria = cargaHoraria;
-    }
-
-    public String getDescricao() {
-        return descricao;
     }
 
     public int getCargaHoraria() {
@@ -31,6 +19,8 @@ public class Curso {
 
     @Override
     public String toString() {
-        return "Curso [titulo=" + titulo + ", descricao=" + descricao + ", cargaHoraria=" + cargaHoraria + "]";
+        return "Curso [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", cargaHoraria=" + cargaHoraria
+                + "]";
     }
+
 }
--- desafio-poo-bootcamp-java/src/br/com/bootcamp/desafio/dominio/Mentoria.java
+++ desafio-poo-bootcamp-java/src/br/com/bootcamp/desafio/dominio/Mentoria.java
@@ -2,30 +2,28 @@ package br.com.bootcamp.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
- String titulo;
- String descricao;
- LocalDate data;
-public String getTitulo() {
-    return titulo;
-}
-public void setTitulo(String titulo) {
-    this.titulo = titulo;
-}
-public void setDescricao(String descricao) {
-    this.descricao = descricao;
-}
-public void setData(LocalDate data) {
-    this.data = data;
-}
-public String getDescricao() {
-    return descricao;
-}
-public LocalDate getData() {
-    return data;
-}
-@Override
-public String toString() {
-    return "Mentoria [titulo=" + titulo + ", descricao=" + descricao + ", data=" + data + "]";
-}
+public class Mentoria extends Conteudo {
+
+    LocalDate data;
+
+    @Override
+    public double calcularXp() {
+       return XP_PADRAO + 20d;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria [titulo=" + getTitulo() + 
+        ", descricao=" + getDescricao() + 
+        ", data=" + data + "]";
+    }
+
 }
