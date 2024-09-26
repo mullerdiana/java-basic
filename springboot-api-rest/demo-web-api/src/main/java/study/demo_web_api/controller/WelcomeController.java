3b4a20c5e428449632da3c640f5a37985e98aa08
Diana Müller <diana.muller@globalhitss.com.br>
2024-09-26 14:40:59 -0300

    Componente REST - @RestController e método Get - @GetMapping

---
+++ springboot-api-rest/demo-web-api/src/main/java/study/demo_web_api/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package study.demo_web_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping
+    public String welcome() {
+        return "welcome to My Spring Boot Web API";
+    }
+}
